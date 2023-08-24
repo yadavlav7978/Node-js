00c632f32c182dba6aa1ab110372d6185ee5fdfc
Lav Yadav <yadavlav571@gmail.com>
2023-08-25 02:32:39 +0530

    CRUD operation in Node Js

--- 1- ( FS Module in NodeJS )/index.js
+++ 1- ( FS Module in NodeJS )/index.js
@@ -7,34 +7,34 @@ const fs=require('fs');
 */}
 
 //! ---------------------------------------- Create directory or folder -----------------------------------------------------
-//fs.mkdirSync('Nodefile'); 
+fs.mkdirSync('Nodefile'); 
 //? fs.mkdirSync() used to create new folder or directory
 
 //! ---------------------------------------------- write Data ---------------------------------------------------------------
 
-//fs.writeFileSync('Nodefile/file.txt','Hi ,My Name is Lav Yadav .'); 
+fs.writeFileSync('Nodefile/file.txt','Hi ,My Name is Lav Yadav .'); 
 //? It write data into file (file.txt) which present in folder NodeFile , if file not present in the specify path then it create new file and add data into the file.
 
 //*---------------------- Override data -----------
 
-//fs.writeFileSync('Nodefile/file.txt','This text override the previous text .');
+fs.writeFileSync('Nodefile/file.txt','This text override the previous text .');
 //? If file present and it contain the data , then it overrides existing data .
 
 //*---------------------- Append data -----------
 
-// fs.appendFileSync('Nodefile/file.txt','  This text append with the previous text .');
+ fs.appendFileSync('Nodefile/file.txt','  This text append with the previous text .');
 //? fs.appendFileSync() funtion append the data in the file . 
 
 //! ---------------------------------------------- Read Data ---------------------------------------------------------------
-//const buffer_data=fs.readFileSync('Nodefile/file.txt'); 
+const buffer_data=fs.readFileSync('Nodefile/file.txt'); 
 //? fs.readFileSync () read the content of file ,it return the binary data of file , if we want orginal data than we need to convert biffer data into string . 
 
-//console.log(buffer_data);                               
-//const Orginal_data=buffer_data.toString(); // Convert Binary data into orginal data
-//console.log(Orginal_data);
+console.log(buffer_data);                               
+const Orginal_data=buffer_data.toString(); // Convert Binary data into orginal data
+console.log(Orginal_data);
 
 //!-------------------------------------------------Rename the file --------------------------------------------------------
-// fs.renameSync('Nodefile/file.txt','Nodefile/Text.txt');
+fs.renameSync('Nodefile/file.txt','Nodefile/Text.txt');
 
 //? fs.renameSync() used to rename the file 
 
